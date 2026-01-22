ee56963ea9e60ed260aa409abc8e7bee681a0fa3
adityapatel2202 <95638852+adityapatel2202@users.noreply.github.com>
2026-01-22 12:51:26 +0000

    Vercel deploy config

--- app/config.py
+++ app/config.py
@@ -2,8 +2,13 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATAmysql://${{MYSQLUSER}}:${{MYSQL_ROOT_PASSWORD}}@${{RAILWAY_PRIVATE_DOMAIN}}:3306/${{MYSQL_DATABASE}}BASE_URL") or (
-        "sqlite:///" + os.path.join(basedir, "app.db")
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+pymysql://{os.environ.get('MYSQLUSER')}:"
+        f"{os.environ.get('MYSQLPASSWORD')}@"
+        f"{os.environ.get('MYSQLHOST')}:"
+        f"{os.environ.get('MYSQLPORT')}/"
+        f"{os.environ.get('MYSQLDATABASE')}"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
