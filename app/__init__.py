4f41fd6ed28d2154cba10d94c7538485ed72d7ed
adityapatel2202 <95638852+adityapatel2202@users.noreply.github.com>
2026-01-22 13:01:24 +0000

    Fix: remove venv and add requirements

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from app.config import Config
+
 
 db = SQLAlchemy()
 migrate = Migrate()
--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://{os.environ.get('MYSQLUSER')}:"
+        f"mysql+pymysql://{os.environ.get('mysql://root:AzVeNRlfwNyZcUMovoFavZCMUOIWjMBL@yamabiko.proxy.rlwy.net:17775/railway')}:"
         f"{os.environ.get('MYSQLPASSWORD')}@"
         f"{os.environ.get('MYSQLHOST')}:"
         f"{os.environ.get('MYSQLPORT')}/"
