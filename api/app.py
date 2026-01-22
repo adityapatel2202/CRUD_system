e66c43034e2a534248854cc3ab683b83d8462378
adityapatel2202 <95638852+adityapatel2202@users.noreply.github.com>
2026-01-22 12:35:51 +0000

    Vercel deploy config

---
+++ api/app.py
@@ -0,0 +1,4 @@
+from run import app
+
+def handler(request):
+    return app
--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(
-        basedir, "..", "instance", "hrms.db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATAmysql://${{MYSQLUSER}}:${{MYSQL_ROOT_PASSWORD}}@${{RAILWAY_PRIVATE_DOMAIN}}:3306/${{MYSQL_DATABASE}}BASE_URL") or (
+        "sqlite:///" + os.path.join(basedir, "app.db")
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
