09e0a87ee066950020b24c8c40bbf7517561b8dc
adityapatel2202 <95638852+adityapatel2202@users.noreply.github.com>
2026-01-23 14:25:37 +0000

    database

--- app/config.py
+++ app/config.py
@@ -1,17 +1,8 @@
-# import os
-
-# basedir = os.path.abspath(os.path.dirname(__file__))
-
-
-# class Config:
-#     SQLALCHEMY_DATABASE_URI = "sqlite:///hrms.db"
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = (
-       f"mysql+pymysql://root:AzVeNRlfwNyZcUMovoFavZCMUOIWjMBL@mysql.railway.internal:3306/railway"
-    )
+    SQLALCHEMY_DATABASE_URI = "sqlite:///hrms.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
