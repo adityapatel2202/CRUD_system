007f10ead92e6d174ffa8c72596d8fd57b74fc21
adityapatel2202 <95638852+adityapatel2202@users.noreply.github.com>
2026-01-23 14:07:48 +0000

    database

--- app/config.py
+++ app/config.py
@@ -12,7 +12,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://{os.getenv('root')}:{os.getenv('AzVeNRlfwNyZcUMovoFavZCMUOIWjMBL')}"
-        f"@{os.getenv('mysql.railway.internal')}:{os.getenv('3306')}/{os.getenv('railway')}"
+       "mysql://root:AzVeNRlfwNyZcUMovoFavZCMUOIWjMBL@mysql.railway.internal:3306/railway"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
