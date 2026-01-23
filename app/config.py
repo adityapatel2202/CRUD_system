7464a1102064dc444643fa601665f8bab87e7b85
adityapatel2202 <95638852+adityapatel2202@users.noreply.github.com>
2026-01-23 13:25:02 +0000

    delete files\

--- app/config.py
+++ app/config.py
@@ -4,11 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://{os.environ.get('mysql://root:AzVeNRlfwNyZcUMovoFavZCMUOIWjMBL@yamabiko.proxy.rlwy.net:17775/railway')}:"
-        f"{os.environ.get('MYSQLPASSWORD')}@"
-        f"{os.environ.get('MYSQLHOST')}:"
-        f"{os.environ.get('MYSQLPORT')}/"
-        f"{os.environ.get('MYSQLDATABASE')}"
-    )
+    SQLALCHEMY_DATABASE_URI = "sqlite:///hrms.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
